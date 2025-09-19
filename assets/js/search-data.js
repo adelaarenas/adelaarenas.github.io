b698b6c5881536f096b1ea469ed8132fc9f948cf
adelaarenas <60279818+adelaarenas@users.noreply.github.com>
2025-09-19 06:34:43 +0000

    Deploying to gh-pages from @ adelaarenas/adelaarenas.github.io@7960b94a7abe262d944763dfbc2fc5c418276e4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,14 +456,14 @@ ninja.data = [{
           description: "An Internship Project - Calibration of NaI(Tl) Scintillation Detector",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-my-undergraduate-thesis",
-          title: 'My Undergraduate Thesis',
+            },},{id: "projects-undergraduate-thesis",
+          title: 'Undergraduate Thesis',
           description: "Evaluation of Neutron Spectra in Different LINAC Configurations Using Modified Bonner Spheres",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-sir-model",
+          title: 'SIR Model',
+          description: "SIR Model with Hospitalization and Death",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
