d770dd9a1f4d9d8961918ac31c4753e261993b8f
adelaarenas <60279818+adelaarenas@users.noreply.github.com>
2025-09-19 07:21:07 +0000

    Deploying to gh-pages from @ adelaarenas/adelaarenas.github.io@3ce5844cac73567ee943e0ef42072bdb9f4c504d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -471,9 +471,9 @@ ninja.data = [{
           description: "Feedbacks of Western North Pacific Tropical Cyclone Properties on ENSO",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-optics-light-and-lasers",
+          title: 'Optics, Light, and Lasers',
+          description: "A group capstone project in determining water content of different liquids using a DIY laser refractometer.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
